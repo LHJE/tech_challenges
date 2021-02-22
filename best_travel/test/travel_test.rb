70eaf175f440b201d4bd4d87a6f29ca25a98accf
LHJE <theeluke@gmail.com>
2021-02-22 11:25:08 -0700

    Add tests to test for nil value result if any data is incorrect

--- best_travel/test/travel_test.rb
+++ best_travel/test/travel_test.rb
@@ -14,14 +14,39 @@ class TravelTest < Minitest::Test
   def test_best_sum_broken
     ls = []
 
-    assert_equal nil, @travel.choose_best_sum(174, 3, ls)
+    assert_nil @travel.choose_best_sum(174, 3, ls)
   end
 
-  def test_best_sum
+  def test_best_sum_broken_two
     ls = [50, 55, 57, 58, 60]
 
-    assert_equal 173, @travel.choose_best_sum(174, 3, ls)
+    assert_nil @travel.choose_best_sum(nil, 3, ls)
   end
+
+  def test_best_sum_broken_three
+    ls = [50, 55, 57, 58, 60]
+
+    assert_nil @travel.choose_best_sum(-1, 3, ls)
+  end
+
+  def test_best_sum_broken_four
+    ls = [50, 55, 57, 58, 60]
+
+    assert_nil @travel.choose_best_sum(174, nil, ls)
+  end
+
+  def test_best_sum_broken_five
+    ls = [50, 55, 57, 58, 60]
+
+    assert_nil @travel.choose_best_sum(174, 0, ls)
+  end
+
+  def test_best_sum_broken_six
+    ls = nil
+
+    assert_nil @travel.choose_best_sum(174, 0, ls)
+  end
+
 
 
 end
