d656320657c995f3a2bbf19893c0b9e6c5133187
LHJE <theeluke@gmail.com>
2021-02-22 12:17:30 -0700

    Update best_sum_helper with working logic for 5 distances

--- best_travel/lib/travel.rb
+++ best_travel/lib/travel.rb
@@ -12,8 +12,15 @@ class Travel
   end
 
   def best_sum_helper(max_sum, max_towns, distances)
-
-
+    distances_2 = distances.rotate(1) + distances.rotate(2)
+    distances_3 = distances.rotate(2) + distances.rotate(3)
+    sums_arrays = (distances + distances).zip(distances_2).zip(distances_3)
+    sums = sums_arrays.map do |sum_array|
+      sum_array.flatten.sum
+    end
+    sums.sort.reverse.find do |sum|
+      sum <= max_sum
+    end
   end
 
 end
