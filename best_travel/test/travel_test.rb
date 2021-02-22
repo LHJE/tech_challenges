a48d2c5ed2eb47385ec3dbdab1e9fa98933ea922
LHJE <theeluke@gmail.com>
2021-02-22 11:18:45 -0700

    Add tests to test for nil value result if town distance list is empty

--- best_travel/test/travel_test.rb
+++ best_travel/test/travel_test.rb
@@ -11,5 +11,17 @@ class TravelTest < Minitest::Test
     assert_instance_of Travel, @travel
   end
 
+  def test_best_sum_broken
+    ls = []
+
+    assert_equal nil, @travel.choose_best_sum(174, 3, ls)
+  end
+
+  def test_best_sum
+    ls = [50, 55, 57, 58, 60]
+
+    assert_equal 173, @travel.choose_best_sum(174, 3, ls)
+  end
+
 
 end
