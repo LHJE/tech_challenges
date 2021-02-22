10b009295294dc5e89fb4b305f3f703bb21e7f28
LHJE <theeluke@gmail.com>
2021-02-22 11:26:08 -0700

    Add choose_best_sum method with conditional to test if any ... data being sent in is bad

--- best_travel/lib/travel.rb
+++ best_travel/lib/travel.rb
@@ -3,5 +3,17 @@ class Travel
   def initialize
   end
 
+  def choose_best_sum(max_sum, max_towns, distances)
+    if max_sum.nil? || max_sum < 0 || max_towns.nil? || max_towns < 1 || distances.nil? || distances == []
+      nil
+    else
+      best_sum_helper(max_sum, max_towns, distances)
+    end
+  end
+
+  def best_sum_helper(max_sum, max_towns, distances)
+
+
+  end
 
 end
