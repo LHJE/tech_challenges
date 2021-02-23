0104ff81ab818f2fdd0b470981f905cc60a67ad7
LHJE <theeluke@gmail.com>
2021-02-22 17:32:58 -0700

    Adding a helper method to sus out what might be a solution

--- best_travel/lib/travel.rb
+++ best_travel/lib/travel.rb
@@ -14,19 +14,45 @@ class Travel
   def best_sum_helper(max_sum, max_towns, distances)
     id = 1
     sum_hash = {}
-    sum_hash[id] = (distances + distances)
+    sum_hash[id] = (distances * (distances.length / 2))
     (max_towns - 1).times {
       id += 1
-      sum_hash[id] = (distances.rotate(id - 1) + distances.rotate(id))
+      sum_hash[id] = distance_rows(distances, id, (distances.length / 2))
     }
     header, *rows = sum_hash.values
     sums_arrays = header.zip(*rows)
     sums = sums_arrays.map do |sum_array|
       sum_array.flatten.sum
-    end
+    end.uniq
     sums.sort.reverse.find do |sum|
       sum <= max_sum
     end
+  end
+
+  def distance_rows(distances, id, number, rotated_distances = [])
+    require "pry"; binding.pry
+    if rotated_distances.length != distances.length * number && rotated_distances.length != distances.length
+      rotated_distances = distances.rotate(id)
+      id += 1
+      rotated_distances << distances.rotate(id)
+      if rotated_distances.flatten.length == distances.length * number
+        rotated_distances.flatten
+      else
+        distance_rows(distances, id, number, rotated_distances.flatten)
+      end
+    elsif rotated_distances.length != distances.length * number
+      id += 1
+      rotated_distances << distances.rotate(id)
+      distance_rows(distances, id, number, rotated_distances.flatten)
+    else
+      rotated_distances.flatten
+    end
+    #
+    #
+    # distances.rotate(id) + distances.rotate(id + 1) + distances.rotate(id + 2)
+
+
+
   end
 
 end
