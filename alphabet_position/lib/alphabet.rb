a0e6a4423f09529f16dc107d3c9b43120c16cf7e
LHJE <theeluke@gmail.com>
2021-02-23 11:15:55 -0700

    Add solution

--- alphabet_position/lib/alphabet.rb
+++ alphabet_position/lib/alphabet.rb
@@ -4,6 +4,9 @@ class Alphabet
   end
 
   def alphabet_position(text)
-
+    alphabet = ("abcdefghijklmnopqrstuvwxyz").split('')
+    text.downcase.gsub!(/[^a-z]/, '').split('').map do |character|
+      alphabet.index(character) + 1
+    end.to_s.tr('[],', '')
   end
 end
