ffec7f5d182608cd57bf8f1a8a275f12cf92d075
LHJE <theeluke@gmail.com>
2021-02-23 11:15:43 -0700

    Add two example tests

--- alphabet_position/test/alphabet_test.rb
+++ alphabet_position/test/alphabet_test.rb
@@ -11,8 +11,12 @@ class AverageTest < Minitest::Test
     assert_instance_of Alphabet, @alphabet
   end
 
-  def test_aveage_a
+  def test_alpha_pos_one
     assert_equal "20 8 5 19 21 14 19 5 20 19 5 20 19 1 20 20 23 5 12 22 5 15 3 12 15 3 11", @alphabet.alphabet_position("The sunset sets at twelve o' clock.")
+  end
+
+  def test_alpha_pos_two
+    assert_equal "", @alphabet.alphabet_position("-.-'")
   end
 
 end
