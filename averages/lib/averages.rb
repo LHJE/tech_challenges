d5397132c09d7fcee619ae66419d81f828f1d6b3
LHJE <theeluke@gmail.com>
2021-02-23 10:35:44 -0700

    Update averages with solution

--- averages/lib/averages.rb
+++ averages/lib/averages.rb
@@ -4,8 +4,16 @@ class Average
   end
 
   def get_grade(one, two, three)
-
+    if (one + two + three) / 3 >= 90
+      "A"
+    elsif (one + two + three) / 3 < 60
+      "F"
+    elsif (one + two + three) / 3 < 70
+      "D"
+    elsif (one + two + three) / 3 < 80
+      "C"
+    else
+      "B"
+    end
   end
-
-
 end
