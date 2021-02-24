b6f5c10d6c6da1464250788ba6a74aa4f51b189c
LHJE <theeluke@gmail.com>
2021-02-24 16:02:19 -0700

    Update Add some experimental logic to play around with

--- snakes_ladders/lib/snakes.rb
+++ snakes_ladders/lib/snakes.rb
@@ -1,6 +1,19 @@
 class Snakes
 
   def initialize
+    @player_1 = true
+    @player_1_position = 0
+    @player_2_position = 0
+  end
+
+  def play(die1, die2)
+
+    if die1 == die2
+      double_turn(die1, die2)
+
+    else
+      regular_turn(die1, die2)
+    end
   end
 
 end
