23214bab5c1b425e97687c4585187a3b3ea91f09
LHJE <theeluke@gmail.com>
2021-02-24 11:53:50 -0700

    Add morse code dictionary and starting logic

--- morse_code/lib/code.rb
+++ morse_code/lib/code.rb
@@ -1,9 +1,61 @@
 class Code
 
-  def initialize
-  end
+  def self.morse_dict
+		{
+														"a" => ".-",
+														"b" => "-...",
+														"c" => "-.-.",
+														"d" => "-..",
+														"e" => ".",
+														"f" => "..-.",
+														"g" => "--.",
+														"h" => "....",
+														"i" => "..",
+														"j" => ".---",
+														"k" => "-.-",
+														"l" => ".-..",
+														"m" => "--",
+														"n" => "-.",
+														"o" => "---",
+														"p" => ".--.",
+														"q" => "--.-",
+														"r" => ".-.",
+														"s" => "...",
+														"t" => "-",
+														"u" => "..-",
+														"v" => "...-",
+														"w" => ".--",
+														"x" => "-..-",
+														"y" => "-.--",
+														"z" => "--..",
+														" " => " ",
+														"1" => ".----",
+														"2" => "..---",
+														"3" => "...--",
+														"4" => "....-",
+														"5" => ".....",
+														"6" => "-....",
+														"7" => "--...",
+														"8" => "---..",
+														"9" => "----.",
+														"0" => "-----"
+													}.invert end
 
   def decodeMorse(morseCode)
+		decoded = []
+		id = 0
+		split_code = morseCode.split(/ /)
+		split_code.each do |character|
+			if character == "" && id != 1
+				decoded << " "
+				id += 1
+			else
+				decoded << Code.morse_dict[character]
+				id = 0
+			end
+		end
+		decoded.join("").upcase
+	end
 
-  end
+
 end
