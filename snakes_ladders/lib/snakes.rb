f7cdc845edc2cc210d53ad2a9f20a416ac82890d
LHJE <theeluke@gmail.com>
2021-02-24 18:52:07 -0700

    Continuing to update branching turns for player 1 or 2, however there... will need to be a lot of refactoring

--- snakes_ladders/lib/snakes.rb
+++ snakes_ladders/lib/snakes.rb
@@ -1,4 +1,4 @@
-class Snakes
+class SnakesLadders
 
   attr_reader :player_1_turn,
               :player_1_position,
@@ -14,62 +14,90 @@ class Snakes
 
   def play(die1, die2)
     if @player_1_turn == true
-      if @player_1_position.nil?
-        @player_1_position = (die1 + die2)
-      else
-        @player_1_position += (die1 + die2)
+      player_1s_turn(die1, die2)
+    else
+      player_2s_turn(die1, die2)
+    end
+  end
+
+  def player_1s_turn(die1, die2)
+    if @player_1_position.nil?
+      @player_1_position = (die1 + die2)
+    else
+      @player_1_position += (die1 + die2)
+    end
+    if die1 == die2
+      if @player_1_position == 2
+        land_on_two
       end
-      if die1 == die2
-        if @player_1_position == 2
-          land_on_two
-        end
+    else
+      if @player_1_position == 2
+        land_on_two
+        @player_1_turn = false
       else
-        if @player_1_position = 2
-          land_on_two
-          @player_1_turn = false
-        end
+        # pretty messy but it can be refactored
+        message = print_message(@player_1_position)
+        @player_1_turn = false
+        message
+      end
+    end
+  end
+
+  def player_2s_turn(die1, die2)
+    if @player_2_position.nil?
+      @player_2_position = (die1 + die2)
+    else
+      @player_2_position += (die1 + die2)
+    end
+    if die1 == die2
+      if @player_2_position == 2
+        land_on_two
+      end
+    else
+      if @player_2_position == 2
+        land_on_two
+        @player_1_turn = true
+      elsif @player_2_position == 8
+          land_on_eight
+          message = print_message(@player_2_position)
+          @player_1_turn = true
+          message
+      else
+        message = print_message(@player_2_position)
+        @player_1_turn = true
+        message
       end
     end
   end
 
   def print_message(player_position)
     if @player_1_turn == true
-      p "Player 1 is on square #{player_position}"
+      "Player 1 is on square #{player_position}"
     else
-      p "Player 2 is on square #{player_position}"
+      "Player 2 is on square #{player_position}"
     end
   end
 
   def land_on_two
+    if @player_1_turn == true
       @player_1_position = 38
       print_message(38)
+    else
+      @player_2_position = 38
+      print_message(38)
+    end
   end
 
-  #
-  # def play(die1, die2)
-  #   if @player_1_turn == true
-  #     if @player_1_position.nil?
-  #       @player_1_position = [9, (die1 + die2)]
-  #     else
-  #       if @player_1_position[0] == 6
-  #         @player_1_position = [@player_1_position[0], @player_1_position[1] - (die1 + die2)]
-  #
-  #       end
-  #     end
-  #     require "pry"; binding.pry
-  #     if die1 == die2
-  #       if @player_1_position == [9,2]
-  #         land_on_two
-  #       elsif @player_1_position = [5,3]
-  #         land_on_fourty_four
-  #       end
-  #     else
-  #       if @player_1_position = [9,2]
-  #         land_on_two
-  #         @player_1_turn = false
-  #       end
-  #     end
-  #   end
-  # end
+  def land_on_eight
+    if @player_1_turn == true
+      @player_1_position = 31
+      print_message(31)
+    else
+      @player_2_position = 31
+      print_message(31)
+    end
+  end
+
+
 
 end
