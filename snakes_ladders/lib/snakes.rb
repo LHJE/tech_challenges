86a6ae8ab6e5f60a5adf8f0ae0fb290e56d870b7
LHJE <theeluke@gmail.com>
2021-02-24 20:14:13 -0700

    Update snakes with new subclass `Player`

--- snakes_ladders/lib/snakes.rb
+++ snakes_ladders/lib/snakes.rb
@@ -1,5 +1,13 @@
 class SnakesLadders
 
+  class Player
+    attr_reader :position
+
+    def initialize(position)
+      @position = position
+    end
+  end
+
   attr_reader :player_1_turn,
               :player_1_position,
               :player_2_position,
@@ -7,65 +15,66 @@ class SnakesLadders
 
   def initialize
     @player_1_turn = true
-    @player_1_position = nil
-    @player_2_position = nil
+    @player_1 = Player.new(0)
+    @player_2 = Player.new(0)
     @board = array = [*0..100]
   end
 
   def play(die1, die2)
     if @player_1_turn == true
-      player_1s_turn(die1, die2)
+      turn(@player_1, die1, die2)
     else
-      player_2s_turn(die1, die2)
+      turn(@player_2, die1, die2)
     end
   end
 
-  def player_1s_turn(die1, die2)
-    if @player_1_position.nil?
-      @player_1_position = (die1 + die2)
+  def turn(player, die1, die2)
+    require "pry"; binding.pry
+    if player.position.nil?
+      player.position = (die1 + die2)
     else
-      @player_1_position += (die1 + die2)
+      player.position += (die1 + die2)
     end
     if die1 == die2
-      if @player_1_position == 2
-        land_on_2
-      elsif @player_1_position == 46
-        land_on_46
+      if player.position == 2
+        land_on_2(player.position)
+      elsif player.position == 46
+        land_on_46(player.position)
       end
     else
-      if @player_1_position == 2
-        land_on_2
+      if player.position == 2
+        land_on_2(player.position)
         @player_1_turn = false
       else
-        print_player_message(@player_1_position)
+        print_player_message(player.position)
       end
     end
   end
 
-  def player_2s_turn(die1, die2)
-    if @player_2_position.nil?
-      @player_2_position = (die1 + die2)
-    else
-      @player_2_position += (die1 + die2)
-    end
-    if die1 == die2
-      if @player_2_position == 2
-        land_on_2
-      end
-    else
-      if @player_2_position == 2
-        land_on_2
-      elsif @player_2_position == 8
-        land_on_8
-        print_player_message(@player_2_position)
-      elsif @player_2_position == 46
-        land_on_46
-        print_player_message(@player_2_position)
-      else
-        print_player_message(@player_2_position)
-      end
-    end
-  end
+  # def player_2s_turn(player_position, die1, die2)
+  #   if player_position.nil?
+  #     @player_2_position = (die1 + die2)
+  #   else
+  #     @player_2_position += (die1 + die2)
+  #   end
+  #   if die1 == die2
+  #     if @player_2_position == 2
+  #       land_on_2(player_position)
+  #     end
+  #   else
+  #     if @player_2_position == 2
+  #       land_on_2(player_position)
+  #     elsif @player_2_position == 8
+  #       land_on_8(player_position)
+  #       print_player_message(@player_2_position)
+  #     elsif @player_2_position == 46
+  #       land_on_46(player_position)
+  #       print_player_message(@player_2_position)
+  #     else
+  #       print_player_message(@player_2_position)
+  #     end
+  #   end
+  # end
 
   def print_player_message(position)
     message = print_message(position)
@@ -81,16 +90,13 @@ class SnakesLadders
     end
   end
 
-  def land_on_2
-    if @player_1_turn == true
-      @player_1_position = 38
-    else
-      @player_2_position = 38
-    end
+  def land_on_2(player_position)
+    require "pry"; binding.pry
+    player_position = 38
     print_message(38)
   end
 
-  def land_on_8
+  def land_on_8(player_position)
     if @player_1_turn == true
       @player_1_position = 31
     else
@@ -99,7 +105,7 @@ class SnakesLadders
     print_message(31)
   end
 
-  def land_on_46
+  def land_on_46(player_position)
     if @player_1_turn == true
       @player_1_position = 25
     else
