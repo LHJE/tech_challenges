9601ba00d45549fa9a43f89f11c5f8baba0e1107
LHJE <theeluke@gmail.com>
2021-02-24 18:33:53 -0700

    Update test to follow new Array method

--- snakes_ladders/test/snakes_test.rb
+++ snakes_ladders/test/snakes_test.rb
@@ -13,7 +13,7 @@ class SnakesTest < Minitest::Test
 
   def test_play
     assert_equal "Player 1 is on square 38", @snakes.play(1, 1)
-    assert_equal false, @snakes.player_1_turn
+    assert_equal true, @snakes.player_1_turn
     assert_equal 38, @snakes.player_1_position
     assert_nil @snakes.player_2_position
   end
@@ -21,6 +21,9 @@ class SnakesTest < Minitest::Test
   # def test_play_two
   #   assert_equal "Player 1 is on square 38", @snakes.play(1, 1)
   #   assert_equal "Player 1 is on square 44", @snakes.play(1, 5)
+  #   assert_equal false, @snakes.player_1_turn
+  #   assert_equal [5,3], @snakes.player_1_position
+  #   assert_nil @snakes.player_2_position
   # end
   #
   # def test_play_three
