735e7e614c493bdd51eb086bda0385196337460c
LHJE <theeluke@gmail.com>
2021-02-24 20:00:24 -0700

    Update tests so they're all active

--- snakes_ladders/test/snakes_test.rb
+++ snakes_ladders/test/snakes_test.rb
@@ -2,13 +2,13 @@ require 'minitest/autorun'
 require 'minitest/pride'
 require './lib/snakes'
 
-class SnakesTest < Minitest::Test
+class SnakesLaddersTest < Minitest::Test
   def setup
-    @snakes = Snakes.new
+    @snakes = SnakesLadders.new
   end
 
   def test_it_exists
-    assert_instance_of Snakes, @snakes
+    assert_instance_of SnakesLadders, @snakes
   end
 
   def test_play
@@ -17,26 +17,32 @@ class SnakesTest < Minitest::Test
     assert_equal 38, @snakes.player_1_position
     assert_nil @snakes.player_2_position
   end
+  
+  def test_play_two
+    assert_equal "Player 1 is on square 38", @snakes.play(1, 1)
+    assert_equal "Player 1 is on square 44", @snakes.play(1, 5)
+    assert_equal false, @snakes.player_1_turn
+    assert_equal 44, @snakes.player_1_position
+    assert_nil @snakes.player_2_position
+  end
 
-  # def test_play_two
-  #   assert_equal "Player 1 is on square 38", @snakes.play(1, 1)
-  #   assert_equal "Player 1 is on square 44", @snakes.play(1, 5)
-  #   assert_equal false, @snakes.player_1_turn
-  #   assert_equal [5,3], @snakes.player_1_position
-  #   assert_nil @snakes.player_2_position
-  # end
-  #
-  # def test_play_three
-  #   assert_equal "Player 1 is on square 38", @snakes.play(1, 1)
-  #   assert_equal "Player 1 is on square 44", @snakes.play(1, 5)
-  #   assert_equal "Player 2 is on square 31", @snakes.play(6, 2)
-  # end
-  #
-  # def test_play_four
-  #   assert_equal "Player 1 is on square 38", @snakes.play(1, 1)
-  #   assert_equal "Player 1 is on square 44", @snakes.play(1, 5)
-  #   assert_equal "Player 2 is on square 31", @snakes.play(6, 2)
-  #   assert_equal "Player 1 is on square 25", @snakes.play(1, 1)
-  # end
+  def test_play_three
+    assert_equal "Player 1 is on square 38", @snakes.play(1, 1)
+    assert_equal "Player 1 is on square 44", @snakes.play(1, 5)
+    assert_equal "Player 2 is on square 31", @snakes.play(6, 2)
+    assert_equal true, @snakes.player_1_turn
+    assert_equal 31, @snakes.player_2_position
+    assert_equal 44, @snakes.player_1_position
+  end
+
+  def test_play_four
+    assert_equal "Player 1 is on square 38", @snakes.play(1, 1)
+    assert_equal "Player 1 is on square 44", @snakes.play(1, 5)
+    assert_equal "Player 2 is on square 31", @snakes.play(6, 2)
+    assert_equal "Player 1 is on square 25", @snakes.play(1, 1)
+    assert_equal true, @snakes.player_1_turn
+    assert_equal 31, @snakes.player_2_position
+    assert_equal 25, @snakes.player_1_position
+  end
 
 end
