601913e1113e22bde3466ee0b46393cddf05cc93
LHJE <theeluke@gmail.com>
2021-02-24 21:47:20 -0700

    Update snakes to consider winning conditions, refactor land_on methods.. Passing 198 tests, still need to work on 52

--- snakes_ladders/lib/snakes.rb
+++ snakes_ladders/lib/snakes.rb
@@ -18,8 +18,7 @@ class SnakesLadders
 
   attr_reader :player_1_turn,
               :player_1,
-              :player_2,
-              :board
+              :player_2
 
   def initialize
     @player_1_turn = true
@@ -28,32 +27,45 @@ class SnakesLadders
   end
 
   def play(die1, die2)
-    if @player_1_turn == true
-      turn(@player_1, die1, die2)
+    if @player_1.position == 100 || @player_2.position == 100
+      if @player_1.position == 100
+        p "Player 1 Wins!"
+      else
+        p "Player 2 Wins!"
+      end
+      p "Game over!"
     else
-      turn(@player_2, die1, die2)
+      if @player_1_turn == true
+        turn(@player_1, die1, die2)
+      else
+        turn(@player_2, die1, die2)
+      end
     end
   end
 
   def turn(player, die1, die2)
-    if player.position == 0
-      player.change_position(die1 + die2)
-    else
-      player.change_position(die1 + die2)
-    end
+    player.change_position(die1 + die2)
     if die1 == die2
-      where_stopped(player)
-      print_message(player.position)
+      win?(player)
     else
-      where_stopped(player)
-      print_player_message(player.position)
+      message = win?(player)
+      @player_1_turn = !@player_1_turn
+      message
     end
   end
 
-  def print_player_message(position)
-    message = print_message(position)
-    @player_1_turn = !@player_1_turn
-    message
+  def win?(player)
+    if player.position > 100
+      player.change_position(-(player.position - 100))
+      end_turn(player)
+    else
+      end_turn(player)
+    end
+  end
+
+  def end_turn(player)
+    where_stopped(player)
+    print_message(player.position)
   end
 
   def print_message(player_position)
@@ -68,96 +80,46 @@ class SnakesLadders
     if player.position == 2
       player.set_position(38)
     elsif player.position == 7
-      land_on_7(player)
+      player.set_position(14)
     elsif player.position == 8
-      land_on_8(player)
+      player.set_position(31)
     elsif player.position == 15
-      land_on_15(player)
+      player.set_position(26)
+    elsif player.position == 16
+      player.set_position(6)
+    elsif player.position == 21
+      player.set_position(42)
+    elsif player.position == 28
+      player.set_position(84)
+    elsif player.position == 36
+      player.set_position(44)
     elsif player.position == 46
-      land_on_46(player)
+      player.set_position(25)
+    elsif player.position == 49
+      player.set_position(11)
+    elsif player.position == 51
+      player.set_position(67)
+    elsif player.position == 62
+      player.set_position(19)
+    elsif player.position == 64
+      player.set_position(60)
+    elsif player.position == 71
+      player.set_position(91)
+    elsif player.position == 74
+      player.set_position(53)
+    elsif player.position == 78
+      player.set_position(98)
+    elsif player.position == 87
+      player.set_position(94)
+    elsif player.position == 89
+      player.set_position(68)
+    elsif player.position == 92
+      player.set_position(88)
+    elsif player.position == 95
+      player.set_position(75)
+    elsif player.position == 99
+      player.set_position(80)
     end
   end
-
-  def land_on_7(player)
-    player.set_position(14)
-  end
-
-  def land_on_8(player)
-    player.set_position(31)
-  end
-
-  def land_on_15(player)
-    player.set_position(26)
-  end
-
-  def land_on_16(player)
-    player.set_position(6)
-  end
-
-  def land_on_21(player)
-    player.set_position(42)
-  end
-
-  def land_on_28(player)
-    player.set_position(84)
-  end
-
-  def land_on_36(player)
-    player.set_position(44)
-  end
-
-  def land_on_46(player)
-    player.set_position(25)
-  end
-
-  def land_on_49(player)
-    player.set_position(11)
-  end
-
-  def land_on_51(player)
-    player.set_position(67)
-  end
-
-  def land_on_62(player)
-    player.set_position(19)
-  end
-
-  def land_on_64(player)
-    player.set_position(60)
-  end
-
-  def land_on_71(player)
-    player.set_position(91)
-  end
-
-  def land_on_74(player)
-    player.set_position(53)
-  end
-
-  def land_on_78(player)
-    player.set_position(98)
-  end
-
-  def land_on_87(player)
-    player.set_position(94)
-  end
-
-  def land_on_89(player)
-    player.set_position(68)
-  end
-
-  def land_on_92(player)
-    player.set_position(88)
-  end
-
-  def land_on_95(player)
-    player.set_position(75)
-  end
-
-  def land_on_99(player)
-    player.set_position(80)
-  end
-
-
 
 end
