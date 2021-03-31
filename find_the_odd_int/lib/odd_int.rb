36a96f092f391e4edae396b3f11f35712d6b0561
LHJE <theeluke@gmail.com>
2021-03-31 13:53:10 -0600

    Update odd_int find_it method, removing result object, changing ... .each to .find

--- find_the_odd_int/lib/odd_int.rb
+++ find_the_odd_int/lib/odd_int.rb
@@ -1,15 +1,13 @@
 class OddInt
 
   def find_it(array)
-    id = 0
-    result = 0
-    array.sort.each do |unit|
-      if unit == array.sort[id + -1] && unit == array.sort[id + +1] || unit != array.sort[id + -1] && unit != array.sort[id + +1]
-        result += unit
-      end
+    id = -1
+    array.sort.find do |unit|
       id += 1
+      if unit == array.sort[id + -1] && unit == array.sort[id + +1] || unit != array.sort[id + -1] && unit != array.sort[id + +1]
+        unit
+      end
     end
-    result
   end
 
 end
