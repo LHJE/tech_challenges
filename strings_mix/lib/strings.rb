e8fbf6b80fc3edb9324b93ba2dc9505230591872
LHJE <theeluke@gmail.com>
2021-03-31 16:21:22 -0600

    Update strings with different approach to arrays

--- strings_mix/lib/strings.rb
+++ strings_mix/lib/strings.rb
@@ -1,38 +1,15 @@
 class Strings
 
   def mix(s1, s2)
-    s1_array = s1.delete('^a-z').split('').sort
-    s2_array = s2.delete('^a-z').split('').sort
-    s1_hash = {}
-    s2_hash = {}
-    result_hash = {"1" => "", "2" => "", "=:" => ""}
-    s1_array.uniq.each do |character|
-      s1_hash[character] = s1_array.count(character)
+    s1_array = s1.delete('^a-z').split('').sort.join('').chars.chunk(&:itself).map{|x|x.last.join}
+    s2_array = s2.delete('^a-z').split('').sort.join('').chars.chunk(&:itself).map{|x|x.last.join}
+
+    s1_array.delete_if do |letters|
+      letters.length == 1
     end
-
-    s2_array.uniq.each do |character|
-      s2_hash[character] = s2_array.count(character)
+    s2_array.delete_if do |letters|
+      letters.length == 1
     end
-
-    s1_hash.each do |letter, amount|
-      if !s2_hash[letter].nil?
-        if amount > s2_hash[letter]
-          amount.times do
-            result_hash["1"] += letter
-          end
-        elsif amount < s2_hash[letter]
-          s2_hash[letter].times do
-            result_hash["2"] += letter
-          end
-        elsif amount = s2_hash[letter]
-          amount.times do
-            result_hash["=:"] += letter
-          end
-        end
-      end
-    end
-
-
 
     require "pry"; binding.pry
 
