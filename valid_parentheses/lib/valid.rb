e532678e76370912d2d846d3ecec7474afd64298
LHJE <theeluke@gmail.com>
2021-03-31 15:06:52 -0600

    Update valid_parentheses method, condensing conditional

--- valid_parentheses/lib/valid.rb
+++ valid_parentheses/lib/valid.rb
@@ -1,25 +1,17 @@
 class Valid
-
   def valid_parentheses(string)
-    simple_string = string.delete('^()').split("")
-    if simple_string.length % 2 == 1 || simple_string[0] == ")" || simple_string.count("(") != simple_string.count(")")
-      false
-    elsif simple_string == []
-      true
-    else
-      id = 0
-      result = true
-      simple_string.each do |character|
-        if character == "("
-          id += 1
-        elsif character == ")" && id != 0
-          id -= 1
-        else
-          result = false
-        end
+    simple_string = string.delete('^()').split('')
+    id = 0
+    result = true
+    simple_string.each do |character|
+      if character == '(' && simple_string.length != 1 && simple_string.length.even? && simple_string.count('(') == simple_string.count(')')
+        id += 1
+      elsif character == ')' && id != 0
+        id -= 1
+      else
+        result = false
       end
-      result
     end
+    result
   end
-
 end
