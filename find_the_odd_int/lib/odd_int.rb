66f828b85c6e0215b11fb3cb41173779a9946abf
LHJE <theeluke@gmail.com>
2021-03-31 13:47:04 -0600

    Update odd_int find_it method.  When trying to see if numbers on ... either side of the specific unit being tested, it's not calculating the id the way I would want.

--- find_the_odd_int/lib/odd_int.rb
+++ find_the_odd_int/lib/odd_int.rb
@@ -1,5 +1,12 @@
 class OddInt
 
-
+  def find_it(array)
+    array.sort.each do |unit|
+      require "pry"; binding.pry
+      if unit == array.sort[array.sort.index(unit) + -1] && unit == array.sort[array.sort.index(unit) + +1] || unit != array.sort[array.sort.index(unit) + -1] && unit != array.sort[array.sort.index(unit) + +1]
+        require "pry"; binding.pry
+      end
+    end
+  end
 
 end
