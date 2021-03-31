9eb726daeeaf56465545c1b76029cadf3f4c20e5
LHJE <theeluke@gmail.com>
2021-03-31 14:43:04 -0600

    Update valid to weed out uneven numbers of ")"'s and "("'s

--- valid_parentheses/lib/valid.rb
+++ valid_parentheses/lib/valid.rb
@@ -1,10 +1,10 @@
 class Valid
 
   def valid_parentheses(string)
-    simple_string = string.delete('^()')
-    if simple_string.length % 2 == 1 || simple_string[0] == ")"
+    simple_string = string.delete('^()').split("")
+    if simple_string.length % 2 == 1 || simple_string[0] == ")" || simple_string.count("(") != simple_string.count(")")
       false
-    elsif simple_string == ""
+    elsif simple_string == []
       true
     else
       require "pry"; binding.pry
