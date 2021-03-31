8d70eecbca4cca2f0a5f3da1cd41311efb1c0c43
LHJE <theeluke@gmail.com>
2021-03-31 16:09:24 -0600

    Update strings with method for filling the results hash.

--- strings_mix/lib/strings.rb
+++ strings_mix/lib/strings.rb
@@ -5,6 +5,7 @@ class Strings
     s2_array = s2.delete('^a-z').split('').sort
     s1_hash = {}
     s2_hash = {}
+    result_hash = {"1" => "", "2" => "", "=:" => ""}
     s1_array.uniq.each do |character|
       s1_hash[character] = s1_array.count(character)
     end
@@ -13,7 +14,28 @@ class Strings
       s2_hash[character] = s2_array.count(character)
     end
 
+    s1_hash.each do |letter, amount|
+      if !s2_hash[letter].nil?
+        if amount > s2_hash[letter]
+          amount.times do
+            result_hash["1"] += letter
+          end
+        elsif amount < s2_hash[letter]
+          s2_hash[letter].times do
+            result_hash["2"] += letter
+          end
+        elsif amount = s2_hash[letter]
+          amount.times do
+            result_hash["=:"] += letter
+          end
+        end
+      end
+    end
+
+
+
     require "pry"; binding.pry
+
   end
 
 end
