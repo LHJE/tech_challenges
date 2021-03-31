4ba2af11a308da20e6c23cb86d1374affb7ddf13
LHJE <theeluke@gmail.com>
2021-03-31 14:35:32 -0600

    Update valid with logic weeding out any future strings that start ... with ")".

--- valid_parentheses/lib/valid.rb
+++ valid_parentheses/lib/valid.rb
@@ -2,7 +2,7 @@ class Valid
 
   def valid_parentheses(string)
     simple_string = string.delete('^()')
-    if simple_string.length % 2 == 1
+    if simple_string.length % 2 == 1 || simple_string[0] == ")"
       false
     else
       require "pry"; binding.pry
