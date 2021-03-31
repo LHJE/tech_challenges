2f0ef55df3b4b2fb4577d240b8d820e0ca884be2
LHJE <theeluke@gmail.com>
2021-03-31 15:44:46 -0600

    Update strings mix method to make hashes

--- strings_mix/lib/strings.rb
+++ strings_mix/lib/strings.rb
@@ -3,7 +3,15 @@ class Strings
   def mix(s1, s2)
     s1_array = s1.delete('^a-z').split('').sort
     s2_array = s2.delete('^a-z').split('').sort
+    s1_hash = {}
+    s2_hash = {}
+    s1_array.uniq.each do |character|
+      s1_hash[character] = s1_array.count(character)
+    end
 
+    s2_array.uniq.each do |character|
+      s2_hash[character] = s2_array.count(character)
+    end
 
     require "pry"; binding.pry
   end
