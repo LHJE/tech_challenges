54247fe1e743dca737cdd966019b9a103097abad
LHJE <theeluke@gmail.com>
2021-03-31 14:37:07 -0600

    Update valid to consider if the simplified string is empty

--- valid_parentheses/lib/valid.rb
+++ valid_parentheses/lib/valid.rb
@@ -4,6 +4,8 @@ class Valid
     simple_string = string.delete('^()')
     if simple_string.length % 2 == 1 || simple_string[0] == ")"
       false
+    elsif simple_string == ""
+      true
     else
       require "pry"; binding.pry
     end
