3194ffe3a4fc1b6eb87f3a47f589ae7a287bfaaf
LHJE <theeluke@gmail.com>
2021-03-31 15:40:41 -0600

    Update strings mix method with some starter logic

--- strings_mix/lib/strings.rb
+++ strings_mix/lib/strings.rb
@@ -1,3 +1,11 @@
 class Strings
 
+  def mix(s1, s2)
+    s1_array = s1.delete('^a-z').split('').sort
+    s2_array = s2.delete('^a-z').split('').sort
+
+
+    require "pry"; binding.pry
+  end
+
 end
