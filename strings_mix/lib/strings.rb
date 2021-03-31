24409c5f7373049944806641a53b1be21500a2ab
LHJE <theeluke@gmail.com>
2021-03-31 16:34:27 -0600

    Add 'delete any strings one letter long' method to the one lines for... ecah of the two arrays

--- strings_mix/lib/strings.rb
+++ strings_mix/lib/strings.rb
@@ -1,14 +1,16 @@
 class Strings
 
   def mix(s1, s2)
-    s1_array = s1.delete('^a-z').split('').sort.join('').chars.chunk(&:itself).map{|x|x.last.join}
-    s2_array = s2.delete('^a-z').split('').sort.join('').chars.chunk(&:itself).map{|x|x.last.join}
+    s1_array = s1.delete('^a-z').split('').sort.join('').chars.chunk(&:itself).map{|x|x.last.join}.delete_if {|letters| letters.length == 1 }
+    s2_array = s2.delete('^a-z').split('').sort.join('').chars.chunk(&:itself).map{|x|x.last.join}.delete_if {|letters| letters.length == 1 }
+    results = []
 
-    s1_array.delete_if do |letters|
-      letters.length == 1
+    s1_array.map do |letters|
+      results << "1:#{letters}"
     end
-    s2_array.delete_if do |letters|
-      letters.length == 1
+
+    s2_array.map do |letters|
+      results << "2:#{letters}"
     end
 
     require "pry"; binding.pry
