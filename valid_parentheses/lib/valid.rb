a8c417a6734a032af24072a2ad5d34fee79a0047
LHJE <theeluke@gmail.com>
2021-03-31 14:33:14 -0600

    Update valid with conditional that will likely be rewritten

--- valid_parentheses/lib/valid.rb
+++ valid_parentheses/lib/valid.rb
@@ -1,5 +1,12 @@
 class Valid
 
-
+  def valid_parentheses(string)
+    simple_string = string.delete('^()')
+    if simple_string.length % 2 == 1
+      false
+    else
+      require "pry"; binding.pry
+    end
+  end
 
 end
