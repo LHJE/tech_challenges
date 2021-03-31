acc34dff7077b5ad49487e38accf36ccdf0eecb8
LHJE <theeluke@gmail.com>
2021-03-31 14:02:30 -0600

    Update odd_int find_it method, but it feels like a quick fix instead ... of a larger solution

--- find_the_odd_int/lib/odd_int.rb
+++ find_the_odd_int/lib/odd_int.rb
@@ -1,11 +1,15 @@
 class OddInt
 
   def find_it(array)
-    id = -1
-    array.sort.find do |unit|
-      id += 1
-      if unit == array.sort[id + -1] && unit == array.sort[id + +1] || unit != array.sort[id + -1] && unit != array.sort[id + +1]
-        unit
+    if array.count == 1
+      array[0]
+    else
+      id = -1
+      array.sort.find do |unit|
+        id += 1
+        if unit == array.sort[id + -1] && unit == array.sort[id + +1] || unit != array.sort[id + -1] && unit != array.sort[id + +1]
+          unit
+        end
       end
     end
   end
