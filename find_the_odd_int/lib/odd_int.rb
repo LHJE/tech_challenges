f604f96176713223fde88e45687ce9bc06599d86
LHJE <theeluke@gmail.com>
2021-03-31 14:08:30 -0600

    Update odd_int find_it method. After some research, found that ... you can give the `.count` method a specific attribute and it'll count the number of those in the array. By using this simple method, the problem was solved.

--- find_the_odd_int/lib/odd_int.rb
+++ find_the_odd_int/lib/odd_int.rb
@@ -1,15 +1,9 @@
 class OddInt
 
   def find_it(array)
-    if array.count == 1
-      array[0]
-    else
-      id = -1
-      array.sort.find do |unit|
-        id += 1
-        if unit == array.sort[id + -1] && unit == array.sort[id + +1] || unit != array.sort[id + -1] && unit != array.sort[id + +1]
-          unit
-        end
+    array.sort.find do |unit|
+      if array.count(unit) % 2 == 1
+        unit
       end
     end
   end
