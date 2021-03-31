54fe05ff9c620e2fdb3c5ce8578408a8b0e5af2e
LHJE <theeluke@gmail.com>
2021-03-31 13:50:38 -0600

    Update odd_int find_it method, using set id and result objects

--- find_the_odd_int/lib/odd_int.rb
+++ find_the_odd_int/lib/odd_int.rb
@@ -1,12 +1,15 @@
 class OddInt
 
   def find_it(array)
+    id = 0
+    result = 0
     array.sort.each do |unit|
-      require "pry"; binding.pry
-      if unit == array.sort[array.sort.index(unit) + -1] && unit == array.sort[array.sort.index(unit) + +1] || unit != array.sort[array.sort.index(unit) + -1] && unit != array.sort[array.sort.index(unit) + +1]
-        require "pry"; binding.pry
+      if unit == array.sort[id + -1] && unit == array.sort[id + +1] || unit != array.sort[id + -1] && unit != array.sort[id + +1]
+        result += unit
       end
+      id += 1
     end
+    result
   end
 
 end
