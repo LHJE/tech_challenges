cafcd96de5b0f5e17eb3f500bd353f3bb296b27d
LHJE <theeluke@gmail.com>
2021-03-31 16:47:35 -0600

    Update strings find longest strings, join them. Test passes

--- strings_mix/lib/strings.rb
+++ strings_mix/lib/strings.rb
@@ -4,7 +4,7 @@ class Strings
     s1_array = s1.delete('^a-z').split('').sort.join('').chars.chunk(&:itself).map{|x|x.last.join}.delete_if {|letters| letters.length == 1 }
     s2_array = s2.delete('^a-z').split('').sort.join('').chars.chunk(&:itself).map{|x|x.last.join}.delete_if {|letters| letters.length == 1 }
     results = []
-
+    end_results = []
     s1_array.map do |letters|
       results << "1:#{letters}"
     end
@@ -13,7 +13,18 @@ class Strings
       results << "2:#{letters}"
     end
 
-    require "pry"; binding.pry
+    results.each do |string|
+      to_compare = results.select{|word| word.include?(string[-1])}
+      if to_compare.length == 1
+        end_results << to_compare[0]
+      elsif to_compare[0].length != to_compare[1].length
+        end_results << to_compare.max_by(&:length)
+      else
+        end_results << "=:#{to_compare[0][2..-1]}"
+      end
+    end
+
+    end_results.uniq.sort.join("/")
 
   end
 
