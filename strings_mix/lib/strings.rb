a99da4d7a9e78776542bfa1c71ea0a7be0c74ced
LHJE <theeluke@gmail.com>
2021-03-31 17:57:53 -0600

    Adding result hash to strings mix in method, for sorting purposes

--- strings_mix/lib/strings.rb
+++ strings_mix/lib/strings.rb
@@ -5,6 +5,7 @@ class Strings
     s2_array = s2.delete('^a-z').split('').sort.join('').chars.chunk(&:itself).map{|x|x.last.join}.delete_if {|letters| letters.length == 1 }
     results = []
     end_results = []
+    equals = []
     s1_array.map do |letters|
       results << "1:#{letters}"
     end
@@ -13,19 +14,32 @@ class Strings
       results << "2:#{letters}"
     end
 
-    results.each do |string|
+    results.sort_by(&:length).reverse.each do |string|
       to_compare = results.select{|word| word.include?(string[-1])}
       if to_compare.length == 1
         end_results << to_compare[0]
       elsif to_compare[0].length != to_compare[1].length
         end_results << to_compare.max_by(&:length)
       else
-        end_results << "=:#{to_compare[0][2..-1]}"
+        equals << "=:#{to_compare[0][2..-1]}"
+      end
+    end
+    end_results << equals.uniq.sort
+
+    # end_results.flatten.uniq.sort_by(&:length).reverse
+    result_hash = {}
+    end_results.flatten.uniq.each do |result|
+      if result_hash[result.length - 2].nil?
+
+        result_hash[result.length - 2] = [result]
+      else
+        result_hash[result.length - 2] << result
       end
     end
 
-    end_results.uniq.sort.join("/")
-
+    
+    require "pry"; binding.pry
+    # end_results.flatten.uniq.sort.join("/")
   end
 
 end
