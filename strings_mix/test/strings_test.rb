a66623a2fc99697f3e362543e7af388d1384318c
LHJE <theeluke@gmail.com>
2021-03-31 15:33:15 -0600

    Update test with specific test for mix method

--- strings_mix/test/strings_test.rb
+++ strings_mix/test/strings_test.rb
@@ -11,5 +11,8 @@ class StringsTest < Minitest::Test
     assert_instance_of Strings, @strings
   end
 
+  def test_strings_one
+    assert_equal "2:eeeee/2:yy/=:hh/=:rr", @strings.mix("Are they here", "yes, they are here")
+  end
 
 end
