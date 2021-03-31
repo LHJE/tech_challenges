9e4804edf5bea94f40fb9f6c324b143ccf54c697
LHJE <theeluke@gmail.com>
2021-03-31 15:16:44 -0600

    Update valid after seeing other answers and learning about Regexp.new... and rescue in Ruby

--- valid_parentheses/lib/valid.rb
+++ valid_parentheses/lib/valid.rb
@@ -1,17 +1,5 @@
 class Valid
   def valid_parentheses(string)
-    simple_string = string.delete('^()').split('')
-    id = 0
-    result = true
-    simple_string.each do |character|
-      if character == '(' && simple_string.length != 1 && simple_string.length.even? && simple_string.count('(') == simple_string.count(')')
-        id += 1
-      elsif character == ')' && id != 0
-        id -= 1
-      else
-        result = false
-      end
-    end
-    result
+    ~ Regexp.new(string) == nil rescue false
   end
 end
