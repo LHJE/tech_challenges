d685ee4d73e481ebe0787e720705fc012081bd16
LHJE <theeluke@gmail.com>
2021-03-31 18:01:17 -0600

    Add second test to tests

--- strings_mix/test/strings_test.rb
+++ strings_mix/test/strings_test.rb
@@ -15,4 +15,8 @@ class StringsTest < Minitest::Test
     assert_equal "2:eeeee/2:yy/=:hh/=:rr", @strings.mix("Are they here", "yes, they are here")
   end
 
+  def test_strings_two
+    assert_equal "1:ooo/1:uuu/2:sss/=:nnn/1:ii/2:aa/2:dd/2:ee/=:gg", @strings.mix("looping is fun but dangerous", "less dangerous than coding")
+  end
+
 end
